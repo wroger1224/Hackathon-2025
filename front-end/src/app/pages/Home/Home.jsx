caf883f5dd138ed3508158d1add4aaeac204ddc9
wroger1224 <wroger1224@gmail.com>
2025-04-12 17:34:07 -0400

    widgets update

--- front-end/src/app/pages/Home/Home.jsx
+++ front-end/src/app/pages/Home/Home.jsx
@@ -1,21 +1,50 @@
 import { useSelector } from "react-redux";
+import Widget from "../../../components/common/Widget/Widget";
 import "../../../index.css";
 
 const Home = () => {
-	const { user } = useSelector((state) => state.user);
-	console.log(user);
-	const { profile } = useSelector((state) => state.userProfile);
-	console.log(profile);
+  const { user } = useSelector((state) => state.user);
+  console.log(user);
+  const { profile } = useSelector((state) => state.userProfile);
+  console.log(profile);
 
   return (
-    <div>
-      <h1>Home</h1>
-      <h2>Welcome to Maxxtivity</h2>
-      <h3>
-        Here you can see your progress and your current level. You can also
-        see your current competition and your progress in it.
-      </h3>
-    </div>
+    <>
+      <div id="home-container">
+        <h1>Home</h1>
+        <h2>Welcome to Maxxtivity</h2>
+        <h3>
+          Here you can see your progress and your current level. You can also
+          see your current competition and your progress in it.
+        </h3>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 p-6">
+        <Widget>
+          <h3 className="text-xl font-semibold mb-4">Widget 1</h3>
+          <p>Content goes here</p>
+        </Widget>
+
+        <Widget>
+          <h3 className="text-xl font-semibold mb-4">Widget 2</h3>
+          <p>Content goes here</p>
+        </Widget>
+
+        <Widget>
+          <h3 className="text-xl font-semibold mb-4">Widget 3</h3>
+          <p>Content goes here</p>
+        </Widget>
+
+        <Widget className="md:col-span-2">
+          <h3 className="text-xl font-semibold mb-4">Minutes</h3>
+          <p>This widget spans two columns</p>
+        </Widget>
+
+        <Widget>
+          <h3 className="text-xl font-semibold mb-4">Widget 4</h3>
+          <p>Content goes here</p>
+        </Widget>
+      </div>
+    </>
   );
 };
 
--- front-end/src/components/common/Layout/Layout.jsx
+++ front-end/src/components/common/Layout/Layout.jsx
@@ -1,15 +1,15 @@
-import { Outlet } from "react-router"
-import Header from "../Header/Header"
+import { Outlet } from "react-router";
+import Header from "../Header/Header";
 
 const Layout = () => {
-	return(
-		<div className="min-h-screen bg-gray-50">
-			<Header />
-			<main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
-				<Outlet />
-			</main>
-		</div>
-	)
-}
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <Header />
+      <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
+        <Outlet />
+      </main>
+    </div>
+  );
+};
 
-export default Layout
+export default Layout;
