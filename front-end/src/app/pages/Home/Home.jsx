0b0139ba8a988bd4b06a510d2c74e5c436a94111
wroger1224 <wroger1224@gmail.com>
2025-04-12 18:22:04 -0400

    Fonts!

--- front-end/src/app/pages/Home/Home.jsx
+++ front-end/src/app/pages/Home/Home.jsx
@@ -9,34 +9,37 @@ const Home = () => {
   console.log(profile);
 
   return (
-    <>
+    <main id="home-main">
       <div id="home-container">
-        <h1>Home</h1>
-        <h2>Welcome to Maxxtivity</h2>
-        <h3>
+        <h1>Welcome {profile.name} to Maxxtivity</h1>
+        <h2></h2>
+        <p>
           Here you can see your progress and your current level. You can also
           see your current competition and your progress in it.
-        </h3>
+        </p>
       </div>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 p-6">
+      <div
+        id="home-widgets"
+        className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 p-6"
+      >
         <Widget>
-          <h3 className="text-xl font-semibold mb-4">Widget 1</h3>
+          <h3 className="text-xl font-semibold mb-4">Team</h3>
           <p>Content goes here</p>
         </Widget>
 
         <Widget>
-          <h3 className="text-xl font-semibold mb-4">Widget 2</h3>
+          <h3 className="text-xl font-semibold mb-4">Leaderboard</h3>
           <p>Content goes here</p>
         </Widget>
 
         <Widget>
-          <h3 className="text-xl font-semibold mb-4">Widget 3</h3>
+          <h3 className="text-xl font-semibold mb-4">Add Workout</h3>
           <p>Content goes here</p>
         </Widget>
 
         <Widget className="md:col-span-2">
           <h3 className="text-xl font-semibold mb-4">Minutes</h3>
-          <p>This widget spans two columns</p>
+          <p>progress bar goes here</p>
         </Widget>
 
         <Widget>
@@ -44,7 +47,7 @@ const Home = () => {
           <p>Content goes here</p>
         </Widget>
       </div>
-    </>
+    </main>
   );
 };
 
